309beee1414efc8ecff6fde16afacd25ffa9394b
NurKeinNeid <mralexman3000@gmail.com>
2023-06-25 14:59:27 +0200

    Revert "overlay: Disable navbar packages for now"
    This reverts commit 7cb5cccb28323323dca8855201b27ddbefa79600.

--- overlays.mk
+++ overlays.mk
@@ -147,15 +147,15 @@ PRODUCT_PACKAGES += \
     BrightnessSliderTranslucentOverlay
 
 # Navbar
-#PRODUCT_PACKAGES += \
-#    NavbarAsusOverlay \
-#    NavbarOnePlusOverlay \
-#    NavbarOneUiOverlay \
-#    NavbarTecnoCamonOverlay \
-#    NavbarAndroidOverlay \
-#    NavbarMotoOverlay \
-#    NavbarNexusOverlay \
-#    NavbarOldOverlay \
-#    NavbarSammyOverlay \
-#    NavbarKronicOverlay \
-#    NavbarDoraOverlay
+PRODUCT_PACKAGES += \
+    NavbarAsusOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarTecnoCamonOverlay \
+    NavbarAndroidOverlay \
+    NavbarMotoOverlay \
+    NavbarNexusOverlay \
+    NavbarOldOverlay \
+    NavbarSammyOverlay \
+    NavbarKronicOverlay \
+    NavbarDoraOverlay
